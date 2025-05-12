e263e9af1821d1a14ecce930e7ea5f5b8b1c89be
Raul Santamaria <raulsantamariao@hotmail.com>
2025-05-12 12:32:52 -0500

    feat: add CrmChatMenuPrincipalModel class with initial properties

---
+++ src/oracle/crmChatMenuPrincipal.model.ts
@@ -0,0 +1,11 @@
+export class CrmChatMenuPrincipalModel {
+  constructor(
+    public CODIGO?: number,
+    public DESCRIPCION?: string,
+    public NOM_COR?: string,
+    public ESTADO?: string,
+    public ORDER?: number,
+    public PALABRA?: string,
+  ){
+  }
+}
